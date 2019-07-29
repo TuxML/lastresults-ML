02fb6c136c500dfe15cc98fbf5afa1c84f94e3b0
Mathieu Acher <mathieu.acher@irisa.fr>
2019-07-29 12:25:57 +0200

    plot evolution RF 10-60K

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DIRECTORY=$0
+DIRECTORY=$1
 echo $DIRECTORY
 mkdir $DIRECTORY
+IGRIDA_PATH="macher@igrida-oar-frontend:/temp_dd/igrida-fs1/macher/analysisrf/rf-analysis2/rf-analysis/$DIRECTORY/*"
 scp "macher@igrida-oar-frontend:/temp_dd/igrida-fs1/macher/analysisrf/rf-analysis2/rf-analysis/$DIRECTORY/*" $DIRECTORY
 scp macher@igrida-oar-frontend:/temp_dd/igrida-fs1/macher/analysisrf/rf-analysis2/rf-analysis/config/config.json $DIRECTORY
